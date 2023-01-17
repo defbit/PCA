14b4f898b481065206514995d6e004d06ad0dd11
defbit <115157208+defbit@users.noreply.github.com>
2023-01-17 17:36:50 -0400

    Update programa que lê as notas dos alunos.c

--- prova - 1/programa que lê as notas dos alunos.c
+++ prova - 1/programa que lê as notas dos alunos.c
@@ -1,12 +1,16 @@
+
+@@ -0,0 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
     float nota, maior, menor, soma = 0, cont = 0;
-    do{
+    while (1) {
         printf("Entre com a nota do aluno: ");
         scanf("%f", &nota);
-
+        if (nota < 0) {
+            break;
+        }
         if (cont == 0) {
             maior = nota;
             menor = nota;
@@ -20,7 +24,7 @@ int main() {
         }
         soma += nota;
         cont++;
-    }while(nota>=0);
+    }
     printf("Maior nota: %.2f\n", maior);
     printf("Menor nota: %.2f\n", menor);
     printf("Media da turma: %.2f\n", soma / cont);
