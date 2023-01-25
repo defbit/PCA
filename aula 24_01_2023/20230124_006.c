780bea99767ab9f60a371710b1ec578b1b6cc10e
defbit <115157208+defbit@users.noreply.github.com>
2023-01-25 11:57:33 -0400

    Update 20230124_006.c

--- aula 24_01_2023/20230124_006.c
+++ aula 24_01_2023/20230124_006.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sequenciaPares(int seq[], int n) {
-int pares[n];
-int cont = 0;
-for (int i = 0; i < n; i++){
-    if (seq[i] % 2 == 0){
-        pares[cont] = seq[i];
-        cont++;
+void selectionSort(int vetor[], int n) {
+int i, j, min, aux;
+    for (i = 0; i < n-1; i++) {
+    min = i;
+    for (j = i+1; j < n; j++){
+    if (vetor[j] < vetor[min]){
+         min = j;
     }
 }
-printf("Sequencia de numeros pares:\n");
-    for (int i = 0; i < cont; i++){
-        printf("%d\n", pares[i]);
-    }
+    aux = vetor[i];
+    vetor[i] = vetor[min];
+    vetor[min] = aux;
+}
 }
 
 int main() {
 int n;
-    printf("Insira o tamanho da sequencia: \n");
+    printf("Insira o tamanho do vetor: ");
     scanf("%d", &n);
-    int seq[n];
-    printf("Insira os elementos da sequencia: \n");
+    int vetor[n];
+    printf("Insira os elementos do vetor: \n");
     for (int i = 0; i < n; i++){
-        scanf("%d", &seq[i]);
+        scanf("%d", &vetor[i]);
     }
-sequenciaPares(seq, n);
-return 0;
+    selectionSort(vetor, n);
+    printf("Vetor ordenado: ");
+    for (int i = 0; i < n; i++) {
+    printf("%d ", vetor[i]);
 }
-
+    return 0;
+}
