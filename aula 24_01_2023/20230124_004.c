cc10499693a1279b116848799debffdd7c2ecf46
defbit <115157208+defbit@users.noreply.github.com>
2023-01-25 11:52:21 -0400

    Update 20230124_004.c

--- aula 24_01_2023/20230124_004.c
+++ aula 24_01_2023/20230124_004.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* sequenciaPares(int seq[], int n, int* tamanho) {
+void sequenciaPares(int seq[], int n) {
 int pares[n];
 int cont = 0;
-    for (int i = 0; i < n; i++) {
-        if (seq[i] % 2 == 0){
-            pares[cont] = seq[i];
-            cont++;
-        }
+for (int i = 0; i < n; i++){
+    if (seq[i] % 2 == 0){
+        pares[cont] = seq[i];
+        cont++;
+    }
 }
-    *tamanho = cont;
-     return pares;
+printf("Sequencia de numeros pares:\n");
+    for (int i = 0; i < cont; i++){
+        printf("%d\n", pares[i]);
+    }
 }
 
-int main(){
-int n, seq[n], tamanho;
-    printf("Insira o tamanho da sequencia:");
+int main() {
+int n;
+    printf("Insira o tamanho da sequencia: \n");
     scanf("%d", &n);
-    printf("Insira os elementos da sequencia:");
+    int seq[n];
+    printf("Insira os elementos da sequencia: \n");
     for (int i = 0; i < n; i++){
         scanf("%d", &seq[i]);
     }
-int * pares = sequenciaPares(seq, n, &tamanho);
-    printf("Sequencia de numeros pares:");
-    for (int i = 0; i < tamanho; i++){
-        printf("%d ", pares[i]);
-    }
+sequenciaPares(seq, n);
 return 0;
 }
