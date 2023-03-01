ec95a00d25eac32fa00534cdab53d493fb93679c
defbit <115157208+defbit@users.noreply.github.com>
2023-03-01 18:55:23 -0300

    Update prova3.c

--- prova3.c
+++ prova3.c
@@ -1,93 +1,104 @@
-// Online C compiler to run C program online
 #include <stdio.h>
+#include <string.h>
 
-void acrescentar_ponto_alunas(float notas[], char sexos[], int num_alunas) {
-    for (int i = 0; i < num_alunas; i++) {
-        if (sexos[i] == 'F') {
-            notas[i]++;
+void conta_alunas(char sexo[2][7][1]){
+    int mulheres_1 = 0, mulheres_2 = 0;
+    
+    for(int i=0; i<6; i++){
+        if(sexo[0][0][i] == 'f'){
+            mulheres_1 += 1;
+            
         }
     }
+    for(int i=0; i<6; i++){
+        if(sexo[1][0][i] == 'f'){
+            mulheres_2 += 1;
+        }
+    }
+    if(mulheres_1 > mulheres_2){
+        printf("maior quantidade de mulheres: grupo 1\n");
+    }
+    else{
+        printf("maior quantidade de mulheres: grupo 2\n");
+    }
+}
+
+void selecionadas(char estudantes[2][7][20], char sexo[2][7][1], float notas[2][7][1]){
+    char alunas_selecionadas[2][6][120];
+    printf("selecionadas da equipe 1 p/ projeto: \n");
+    for(int i=0; i<6; i++){
+        if(sexo[0][0][i] == 'f' && notas[0][0][i] >= 8.0){
+            strcpy(alunas_selecionadas[0][i], estudantes[0][i]);
+            printf("%s ", alunas_selecionadas);
+        }
+    }
+    printf("\n");
+    
+    printf("selecionadas da equipe 2 p/ projeto:: \n");
+    for(int i=0; i<6; i++){
+        if(sexo[1][0][i] == 'f' && notas[1][0][i] >= 8.0){
+            strcpy(alunas_selecionadas[1][i], estudantes[1][i]);
+            printf("%s ", alunas_selecionadas);
+        }
+    }
+    printf("\n");
+}
+
+void ponto_extra(char estudantes[2][7][20], float notas[2][7][1]){
+    printf("Novas notas: \n");
+    for(int i=0; i<6; i++){
+        if(notas[1][0][i] < 10){
+            notas[1][0][i] += 1;
+        }
+        printf("%s: %.1f\n", estudantes[1][i], notas[1][0][i]);
+    }
+}
+
+void aluno_novo(char estudantes[2][7][20], char sexo[2][7][1], float notas[2][7][1]){
+    char novo_est[20];
+    char novo_sexo;
+    float nova_nota;
+    int equipe;
+
+    printf("nome do estudante: ");
+    scanf("%s", novo_est);
+    printf("sexo do estudante: ");
+    scanf(" %c", &novo_sexo);
+    printf("nota do estudante: ");
+    scanf("%f", &nova_nota);
+    printf("selecione a equipe (1 ou 2): ");
+    scanf("%d", &equipe);
+    printf("\n");
+
+    strcpy(estudantes[equipe-1][6], novo_est);
+    sexo[equipe-1][0][6] = novo_sexo;
+    notas[equipe-1][0][6] = nova_nota;
+    
+    printf("Nova formação da equipe %d:\n", equipe);
+    for(int i=0; i<7; i++){
+        printf("%s (%c): %.1f\n", estudantes[equipe-1][i], sexo[equipe-1][0][i], notas[equipe-1][0][i]);
+    }
+    
 }
 
 int main() {
-    char nomes[2][6][20] = {
-    {"Ana", "Bianca", "Clara", "Daniela", "Evelyn", "Fabiana"},
-    {"Gabriel", "Henrique", "João", "Lucas", "Miguel", "Pedro"}
-};
-
-
-char sexo[2][6] = {
-    {'F', 'F', 'F', 'F', 'F', 'F'},
-    {'M', 'M', 'M', 'M', 'M', 'M'}
-};
-
-
-float notas[2][6] = {
-    {9.5, 7.0, 8.5, 6.5, 9.0, 7.5},
-    {8.0, 6.5, 7.5, 8.5, 6.0, 9.0}
+    char estudantes[2][7][20] = {
+        {"carla", "helena", "Iana", "Pedro", "Julio", "Marcia", ""},{"Carlos", "lua", "Selina", "paulo", "Alex", "felipe", ""}
+    };
+    char sexo[2][7][1] = {
+        {'f', 'f', 'f', 'm', 'm', 'f', 'n'},{'m', 'f', 'f', 'm', 'm', 'm', 'n'}
+    };
+    float notas[2][7][1] = {
+        {10, 8, 3, 4, 6, 5, 0},{2, 8, 8, 6, 4, 3, 0}
+    };
     
-
-int mulheres_equipe1 = 0, mulheres_equipe2 = 0;
-    for (int i = 0; i < 6; i++) {
-        if (sexo[0][i] == 'F') {
-            mulheres_equipe1++;
-    }
-    if (sexo[1][i] == 'F') {
-        mulheres_equipe2++;
-    }
-}
-
-    if (mulheres_equipe1 > mulheres_equipe2) {
-        printf("A equipe 1 tem mais mulheres.\n");
-} 
-    else if (mulheres_equipe2 > mulheres_equipe1) {
-        printf("A equipe 2 tem mais mulheres.\n");
-} 
-    else {
-        printf("As equipes tem a mesma quantidade de mulheres.\n");
-}
-char alunas_selecionadas[12][20];
-int num_alunas_selecionadas = 0;
-
-void acrescentar_ponto_alunas(float notas[], char sexos[], int num_alunas) {
-    for (int i = 0; i < num_alunas; i++) {
-        if (sexos[i] == 'F') {
-            notas[i]++;
-        }
-    }
-}
-    for (int equipe = 0; equipe < 2; equipe++) {
-        for (int aluno = 0; aluno < 6; aluno++) {
-            if (sexo[equipe][aluno] == 'F' && notas[equipe][aluno] >= 8.0) {
-            strcpy(alunas_selecionadas[num_alunas_selecionadas], nomes[equipe][aluno]);
-                num_alunas_selecionadas++;
-        }
-    }
-}
-
-printf("As seguintes alunas podem participar do projeto:\n");
-    for (int i = 0; i < num_alunas_selecionadas; i++) {
-        printf("%s\n", alunas_selecionadas[i]);
-}
-
-    acrescentar_ponto_alunas(notas_equipe2, sexos_equipe2, num_alunas_equipe2);
-
-void adicionar_aluno(char nomes[], char sexos[], float notas[], int* num_alunos, int max_alunos) {
-    
-    printf("Digite o nome do novo aluno: ");
-    scanf("%s", nomes + (*num_alunos * TAM_NOME));
-
-    printf("Digite o sexo do novo aluno (M/F): ");
-    scanf(" %c", &sexos[*num_alunos]);
-
-    printf("Digite a nota final do novo aluno: ");
-    scanf("%f", &notas[*num_alunos]);
-
-    (*num_alunos)++;
-}
-
-    adicionar_aluno(nomes_equipe1, sexos_equipe1, notas_equipe1, &num_alunas_equipe1, MAX_ALUNOS);
-
+    conta_alunas(sexo);
+    printf("\n");
+    selecionadas(estudantes, sexo, notas);
+    printf("\n");
+    ponto_extra(estudantes, notas);
+    printf("\n");
+    aluno_novo(estudantes, sexo, notas);
 
     return 0;
 }
